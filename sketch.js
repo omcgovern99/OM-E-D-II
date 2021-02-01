ad6cf7dfae70b32337b275b191f97152a0dbaa5c
omcgovern99 <71271309+omcgovern99@users.noreply.github.com>
2021-02-01 13:34:56 -0500

    update

--- sketch.js
+++ sketch.js
@@ -1,35 +1,39 @@
 let img;
 
-function preload (){
-  img = loadImage ('/Images/DayWindow3.png');
+function preload() {
+    img = loadImage("images/DayWindow3.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowWidth);
-noStroke ();
+    createCanvas(windowWidth, windowHeight);
+    noStroke();
 }
 
-function draw() { 
-  
-var num = 4; 
-  
-var XsideLen = windowWidth / num;
+function draw() {
 
- var YsideLen = (windowWidth / num) / 2;
+    var num = 4;
 
-  
-  for (var dy = 0; dy < windowWidth; dy = dy + YsideLen) {
+    var XsideLen = windowWidth / num;
 
-    for (var dx = 0; dx < windowWidth; dx = dx + XsideLen) {
-    
-   quad(dx, dy,
-          dx + XsideLen, dy,
-          dx + XsideLen, dy + YsideLen,
-          dx, dy + YsideLen);
 
-    image(img, dx, dy, XsideLen, YsideLen);
-  } 
-    
-  }
+    for (var dy = 0; dy < windowHeight; dy = dy + XsideLen * 0.53) {
 
+        for (var dx = 0; dx < windowWidth; dx = dx + XsideLen) {
+
+            quad(dx, dy,
+                dx + XsideLen, dy,
+                dx + XsideLen, dy + XsideLen * 0.53,
+                dx, dy + XsideLen * 0.53);
+
+            image(img, dx, dy, XsideLen, XsideLen * 0.53);
+        }
+
+
+    }
+    print(XsideLen);
+    print(XsideLen * 0.53);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
 }
